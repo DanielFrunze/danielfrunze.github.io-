88ee900c3e1aa172102be2ae1e3a4943e048e197
DanielFrunze <148353120+DanielFrunze@users.noreply.github.com>
2024-04-25 15:46:33 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,20 @@ const passwordInput = document.querySelector('#password');
 const repeatPasswordInput = document.querySelector('#repeat-password')
 const submitPasswordButton = document.querySelector('button')
 const mainComponent = document.querySelector('.main-component')
+const searchParams = new URLSearchParams(window.location.search);
+
+const resetPasswordObj = {
+    email: 'frunze01@mail.ru',
+    resetPasswordCode: "3uTf9Rd4qHHEtmHSAVsHtVjcKMe4CdzTHvn5W3uudNV5wtRHM4MRBvgtwqdkydW69y2vgUY3sxp9JGrCPx6duyqs8Aqf3z4Uf5mA",
+    newPassword: 'qwerty123'
+}
+
+if(searchParams.has('email')){
+    resetPasswordObj.email = searchParams.get('email')
+}
+if(searchParams.has('resetcode')){
+    resetPasswordObj.resetPasswordCode = searchParams.get('resetcode')
+}
 
 const modalData = {
     denied: {
@@ -48,13 +62,7 @@ const showPassword = (e) => {
     else passwordInput.type = 'password'
 }
 
-const resetPasswordObj = {
-    email: 'frunze01@mail.ru',
-    resetPasswordCode: "3uTf9Rd4qHHEtmHSAVsHtVjcKMe4CdzTHvn5W3uudNV5wtRHM4MRBvgtwqdkydW69y2vgUY3sxp9JGrCPx6duyqs8Aqf3z4Uf5mA",
-    newPassword: 'qwerty123'
-}
-
-async function resetPassword(resetPasswordObj) {
+const resetPassword = async (resetPasswordObj) => {
     try {
         const response = await fetch("http://127.0.0.1:450/api/v2/User/ResetPassword", {
             method: 'POST',
@@ -67,7 +75,6 @@ async function resetPassword(resetPasswordObj) {
         return data;
     } catch (error) {
         console.error('Error resetting password:', error);
-        // Handle errors appropriately depending on your application's needs
     }
 }
 
@@ -76,75 +83,74 @@ const validatePassword = () => {
     if (errorMessage && errorMessage.tagName === 'P') {
         passwordInput.parentElement.classList.remove('invalid-input')
         repeatPasswordInput.parentElement.classList.remove('invalid-input')
-
+        submitPasswordButton.disabled = false
         errorMessage.remove();
     }
 
     if (!passwordInput.value.match(/^.{8,}$/)) {
-        passwordInput.parentElement.insertAdjacentHTML('afterend', '<p class="validation-failed">8 chars</p>');
+        passwordInput.parentElement.insertAdjacentHTML('afterend', '<p class="validation-failed">The password should contain at least 8 characters. Please retry.</p>');
         passwordInput.parentElement.classList.add('invalid-input');
+        submitPasswordButton.disabled = true
     } else if (passwordInput.value !== repeatPasswordInput.value) {
         repeatPasswordInput.parentElement.classList.add('invalid-input');
         passwordInput.parentElement.classList.add('invalid-input');
-        passwordInput.parentElement.insertAdjacentHTML('afterend', '<p class="validation-failed">not match</p>');
+        passwordInput.parentElement.insertAdjacentHTML('afterend', '<p class="validation-failed">The passwords are not matching. Please retry.</p>');
+        submitPasswordButton.disabled = true
     }
 };
+
+const checkRequestStatus = (requestStatus) => {
+    switch (requestStatus){
+        case 0: {
+            return mainComponent.outerHTML = createModal('success')
+            break;
+        }
+        case -1: {
+            modalData['denied'].message = 'Oops something went wrong.'
+            modalData['denied'].description = 'Please retry';
+            return mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
+            break;
+        }
+        case -2: {
+            modalData['denied'].message = 'Oops something went wrong.'
+            modalData['denied'].description = 'Please retry';
+            mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
+            break;
+        }
+        case -3: {
+            modalData['denied'].description = 'Something went wrong';
+            mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
+            break;
+        }
+        case -4: {
+            modalData['denied'].message = 'Oops something went wrong.'
+            modalData['denied'].description = 'Please retry';
+            mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
+            break;
+        }
+        case -5: {
+            mainComponent.outerHTML = createModal('expired')
+            break;
+        }
+    }
+}
 
 
 const submitForm = async () => {
     this.event.preventDefault()
-    const passwordLength = /^.{8,}$/
-    if(!passwordInput.value.match(passwordLength)) {
-        modalData['denied'].description = 'The password should contain at least 8 characters. Please retry.';
-        mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
-    }
-    else if(passwordInput.value !== repeatPasswordInput.value){
-        modalData['denied'].description = 'The passwords are not matching. Please retry.';
-        mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
-    } else {
-        // const response = fetch("http://127.0.0.1:450/api/v2/User/ResetPassword", {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json;charset=utf-8'
-        //     },
-        //     body: JSON.stringify(resetPasswordObj)
-        // }).then(data => {return data.json()})
-        //     .then(res => res)
+    // const passwordLength = /^.{8,}$/
+    // if(!passwordInput.value.match(passwordLength)) {
+    //     modalData['denied'].description = 'The password should contain at least 8 characters. Please retry.';
+    //     mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
+    // }
+    // else if(passwordInput.value !== repeatPasswordInput.value){
+    //     modalData['denied'].description = 'The passwords are not matching. Please retry.';
+    //     mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
+    // } else {
+        resetPasswordObj.newPassword = passwordInput.value
         const requestStatus = await resetPassword(resetPasswordObj);
-        switch (requestStatus.error){
-            case 0: {
-                mainComponent.outerHTML = createModal('success')
-                break;
-            }
-            case -1: {
-                modalData['denied'].message = 'Oops something went wrong.'
-                modalData['denied'].description = 'Please retry';
-                mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
-                break;
-            }
-            case -2: {
-                modalData['denied'].message = 'Oops something went wrong.'
-                modalData['denied'].description = 'Please retry';
-                mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
-            }
-            case -3: {
-                modalData['denied'].description = 'Something went wrong';
-                mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
-                break;
-            }
-            case -4: {
-                modalData['denied'].message = 'Oops something went wrong.'
-                modalData['denied'].description = 'Please retry';
-                mainComponent.insertAdjacentHTML("afterend", createModal('denied'));
-                break;
-            }
-            case -5: {
-                mainComponent.outerHTML = createModal('expired')
-                break;
-            }
-        }
-        // mainComponent.insertAdjacentHTML("afterend", createModal(modalData['success']));
-    }
+        checkRequestStatus(requestStatus.error)
+    // }
 
     const notificationCloseBtn = document.querySelector('.close-btn')
     if (notificationCloseBtn){
